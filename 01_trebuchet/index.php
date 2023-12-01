23091b7d36faebbb05d2d2d2ebf00c53f029f8c4
paufandos <fandosgorrispau@gmail.com>
2023-12-01 09:52:25 +0100

    feat(01_trebuchet): Getcalibration code

---
+++ 01_trebuchet/index.php
@@ -0,0 +1,22 @@
+<?php
+const FILE = './stars.txt';
+const NUMBERS_REGEX = '/\D/';
+const FIRST_CODE_NUMBER = 0;
+const LAST_CODE_NUMBER = -1;
+
+$calibration_values = array();
+$lines = file(FILE, FILE_IGNORE_NEW_LINES);
+$totalSum = 0;
+
+foreach ($lines as $line) {
+    $numbers = preg_replace(NUMBERS_REGEX, '', $line);
+    $first_digit = $numbers[FIRST_CODE_NUMBER];
+    $last_digit = substr($numbers, LAST_CODE_NUMBER);
+
+    $finalNumberCode = $first_digit . $last_digit;
+    $calibration_values[] = $finalNumberCode;
+
+    $totalSum += intval($finalNumberCode);
+}
+
+echo "El código de calibración és: $totalSum";
